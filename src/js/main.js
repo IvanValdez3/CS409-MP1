80ccf7110f3da3fe5f867d1e58dbc5947680cde7
unknown <LocalAdmin@ENGRIT-LOAN-F02.ad.uillinois.edu>
2025-09-23 19:42:06 -0500

    Was having some issue with dimensions and shrinking, but I think it's fixed now

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,29 @@
 /* Your JS here. */
 console.log('Hello World!')
+
+/*The goal is simple when we scroll the navbar and it's contents should shrink,
+  but return to normal when at the top*/
+window.addEventListener('scroll', function() {
+    const nav = document.getElementById('nav');
+    const scrollPosition = window.scrollY;
+    
+    if (scrollPosition > 50) {
+        nav.classList.add('shrink');
+    } else {
+        nav.classList.remove('shrink');
+    }
+});
+
+document.querySelectorAll('.nav-right a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
+});
